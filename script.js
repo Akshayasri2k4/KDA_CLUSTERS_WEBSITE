8d997d3b9b6cf23f3eb6a34e089aa4c8d0b7397e
Akshayasri2k4 <akshayasrinv@gmail.com>
2026-03-20 21:57:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToServices() {
+    document.getElementById("services").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+function toggleMenu() {
+    document.querySelector("nav").classList.toggle("active");
+}
